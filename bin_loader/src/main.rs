1fa8f6db69fb2b26f743cbed9fe1e11532bef139
NewWorldVulture <49806198+NewWorldVulture@users.noreply.github.com>
2021-07-26 11:20:43 -0700

    Update main.rs

--- bin_loader/src/main.rs
+++ bin_loader/src/main.rs
@@ -21,16 +21,24 @@ fn main() -> io::Result<()> {
     let (vid, pid) = (0x1125, 0x1807);
     let device = api.open(vid, pid).unwrap();
 
+    let mut completed: bool = true;
     for byte in &buffer {
+        // First byte is SET_REPORT ID. We only ever use 0x0
         let sm_buff = [0u8, 1u8, *byte, 0u8, 0u8, 0u8];
         let result = device.send_feature_report(&sm_buff);
 
         match result {
-            Ok(bytes) => { println!("Wrote: {:?} bytes", bytes) },
-            Err(e) => { eprintln!("{:?}", e) },
-    }
+            Ok(_) => { },
+            Err(e) => { eprintln!("{:?}", e);
+                        completed = false; },
+        }
     }
 
+    if completed {
+        println!("Keypad configured.");
+    } else {
+        println!("Keypad configuration not successful.");
+    }
 
     Ok(())
 }

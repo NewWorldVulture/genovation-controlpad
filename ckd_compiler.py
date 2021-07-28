f825cbbd93f5c6fbddc7c312e87091f01dacec10
Ada <49806198+NewWorldVulture@users.noreply.github.com>
2021-07-28 08:01:51 -0700

    Update ckd_compiler.py

--- ckd_compiler.py
+++ ckd_compiler.py
@@ -1,63 +1,69 @@
+#!/usr/bin/python3
+# COPYRIGHT (c) AMAZON.COM. All Rights Reserved
+# Created by Ada MacLurg (maclurb@)
+# Documentation at https://github.com/NewWorldVulture/genovation-controlpad
+
+
 class GlobalOptions:
+	""" Holds Global Settings such as LED Settings and Macro Toggling Keys """
 	def __init__(self, raw_info):
-		self.header_info = [0xE4, 0x01, 0x0C, 0xE0, 0x70]
+		self.file_header = [0xE4, 0x01, 0x0C, 0xE0, 0x70]
 		# The macro shift and toggle keys are not stored in the GLOBAL_PARAMETERS, ...
-		# So we modify these as we iterate through the key macros
+		# ... so we set them to defaults then modify them as we find them in the key macros
 		self.macro_toggle_keys = [0x80, 0x80]
 		self.macro_shift_keys =  [0x80, 0x80]
 
-		# This number isn't used in the actual header information or anywhere explicitly
-		# We'll only need
+		# This number isn't used anywhere explicitly
+		# We only need it to know the number of key macros to compile
 		self.number_of_keys = int(raw_info[2].split('=')[1])
 
-		# default [0x02]
-		self.key_rollover = [int(raw_info[3].split('=')[1], 16)]
-		self.null_spacer = [0x00] # 
-		# default [0x01]
+		self.n_key_rollover = [int(raw_info[3].split('=')[1], 16)]
+		self.null_spacer = [0x00]	# Unknown Function
 		self.char_pacing = [int(raw_info[7].split('=')[1], 16)]
-		# default [0x01, 0x02, 0x04]
 		self.led_functions = [int(raw_info[x].split('=')[-1], 16) for x in (4,5,6)]
 
-		self.fields = [self.header_info,
-									self.macro_toggle_keys,
-									self.macro_shift_keys,
-									self.key_rollover,
-									self.null_spacer,
-									self.char_pacing,
-									self.led_functions,]
+		self.fields = [self.file_header,
+			self.macro_toggle_keys,
+			self.macro_shift_keys,
+			self.n_key_rollover,
+			self.null_spacer,
+			self.char_pacing,
+			self.led_functions,
+		]
 
-	# Expects key_num as aa hex number to be passed in
-	# from string -> int(hex_str, 16)
 	def add_macro_toggle_key(self, key_num):
 		# There are only two slots available for Toggle and Shift keys
 		try:
-			ind = self.macro_toggle_keys.index(0x80)
-			self.macro_toggle_keys[ind] = key_num
+			# Find the first instance of `0x80`. If not found, skip it
+			slot = self.macro_toggle_keys.index(0x80)
+			self.macro_toggle_keys[slot] = key_num
 		except Exception:
 			pass
 
-	# Expects key_num as aa hex number to be passed in
-	# from string -> int(hex_str, 16)
 	def add_macro_shift_key(self, key_num):
+		# There are only two slots available for Toggle and Shift keys
 		try:
-			ind = self.macro_shift_keys.index(0x80)
-			self.macro_shift_keys[ind] = key_num
+			# Find the first instance of`0x80`. If not found, skip it
+			slot = self.macro_shift_keys.index(0x80)
+			self.macro_shift_keys[slot] = key_num
 		except Exception:
 			pass
 
 	def add_compiled_info(self, file):
+		# Write values to
 		for field in self.fields:
 			for val in field:
 				file.write(bytes((val,)))
 
 
 class KeyMacro:
+	""" Holds information for the Macro Data for a Key """
 	# Initial Parsing of Data
 	def __init__(self, raw_info, global_options):
 		self.opening = [0xE4, 0x02]
 		# Parse out digits from line
 		self.keynum = [int(''.join([x for x in raw_info[0] if x.isdigit()]))]
-		# These are all altered by various options. We'll get to it, Together™
+		# These are all altered by various options. We'll get to it, Together
 		self.l1_data_length = []
 		self.l1_macro_options = [0x00]
 		self.l2_data_length = []
@@ -78,18 +84,17 @@ class KeyMacro:
 		# Level 1 Macro Data
 		self.l1_macro_data = self._macro_parser(raw_info[6])
 		if len(self.l1_macro_data) > 1:
-			# If it's an empty string, we leave this blank `[]`
-			# Otherwise, each macro  is pre-pended with the length of its data
+			# If it's an empty string, we leave this blank
+			# Otherwise, each macro is pre-pended with the length of its data
 			self.l1_data_length = [len(self.l1_macro_data)]
 			print(str(self.keynum[0]) +" l1: "+ str(len(self.l1_macro_data)))
-			# The last bit of macro_options is whether any data is written for this macro
+			# The final bit of macro_options is whether any data is written for this macro
 			self.l1_macro_options[0] += 0b0001
 
-		# Returns macro in nice list format
 		self.l2_macro_data = self._macro_parser(raw_info[7])
 		if len(self.l2_macro_data) > 1:
-		# If it's an empty string, we leave this blank `[]`
-			# Otherwise, each macro  is pre-pended with the length of its data
+			# Each macro is pre-pended with the length of its data
+			# ... unless it has a length of 0
 			self.l2_data_length = [len(self.l2_macro_data)]
 			print(str(self.keynum[0]) +" l2: "+ str(len(self.l2_macro_data)))
 			# The last bit of macro_options is whether any data is written for this macro
@@ -104,70 +109,75 @@ class KeyMacro:
 		# The bits flipped here are according to the MacroMode of the macro
 		# These control the first two bits. Mode '0' and Mode '2' are Identical
 		macro_mode_lookup_table = {
-												'0': 0b0000,	# "Default Mode" (changes)
-												'1': 0b1000,	# Separate Up Codes
-												'2': 0b0000,	# Macro Mode
-												'3': 0b0100}	# Literal Mode
+			'0': 0b0000,	# "Default Mode" (changes)
+			'1': 0b1000,	# Separate Up Codes
+			'2': 0b0000,	# Macro Mode
+			'3': 0b0100}	# Literal Mode
 		self.l1_macro_options[0] += macro_mode_lookup_table[raw_info[4].split('=')[1]]
 		self.l2_macro_options[0] += macro_mode_lookup_table[raw_info[5].split('=')[1]]
 
 		self.fields = [self.opening,
-									self.keynum,
-									self.l2_macro_options,
-									self.l1_macro_options,
-									self.keynum,
-									self.l1_data_length,
-									self.l1_macro_data,
-									self.l2_data_length,
-									self.l2_macro_data,
-									]
+			self.keynum,
+			self.l2_macro_options,
+			self.l1_macro_options,
+			self.keynum,
+			self.l1_data_length,
+			self.l1_macro_data,
+			self.l2_data_length,
+			self.l2_macro_data,
+		]
 	
 	def _macro_parser(self, raw_line):
+		""" Parses out the macro into individual bytes and puts it in the format GENOVATION wants """
 		line = raw_line.split('=')[1]
 		# Split string into groups of two characters
 		line = [int(line[x:x+2], 16) for x in range(0, len(line), 2)]
 		# In the compiled files, 0xF0 is replaced with 0xE3. This is undocumented.
-		for c, val in enumerate(line): #!TODO: Fix this!!! it didn't work!!! >:0
+		for c, val in enumerate(line):
 			if val == 0xF0:
 				line[c] = 0xE3
-		# All macros have a null terminator on the end. If the macro is blank, just return a null.
+		# All macros are null terminated. If the macro is blank, just returns a null.
 		line = line + [0x00]
 		return line
 
 	def add_compiled_info(self, file):
+		""" Called to write data to passed-in file """
 		for field in self.fields:
 			for val in field:
 				file.write(bytes((val,)))
 
 
-def main(filename):
+def compile_ckd(i_file=None, o_file=None):
 	global_options = None
 	key_params = []
 
-	with open(filename, "r") as file:
+	with open(i_file, "r") as file:
 		# Buffer holding each line in the .ckd file until we parse it out
 		text_buffer = []
 
 		for line in file.readlines():
-			# Parse out text_buffer one at a time
+			# If we reach a new section, parse out the previous section, ...
+			# ...then write the line to the text buffer
 			if (line.strip().startswith("[") or line.strip() == '') and (len(text_buffer)):
 				if text_buffer[0] == "[GLOBAL_PARAMETERS]":
 					global_options = GlobalOptions(text_buffer)
 				else:
 					key_params.append(KeyMacro(text_buffer, global_options))
-				# Clear the buffer 
+				# Clear the previous section's buffer 
 				text_buffer = []
+			# Append the line to the buffer
 			text_buffer.append(line.strip())
+		
 		if text_buffer:
 			key_params.append(KeyMacro(text_buffer, global_options))
 
-	# Begin Compilation of ckd
-	new_file = filename.split('.')[0] + "_compiled_bin.bin"
-	with open(new_file, "wb") as new_filename:
-		global_options.add_compiled_info(new_filename)
+	# Begin Writing of .bin
+	with open(o_file, "wb") as output_file:
+		# Write Header to File
+		global_options.add_compiled_info(output_file)
+
+		# Write Key Macros to File
 		for key_number in range(global_options.number_of_keys):
-			key_params[key_number].add_compiled_info(new_filename)
-		new_filename.write(bytes((0xE5,))) # Terminator
-
-
-main("sample_ckd.ckd")
+			key_params[key_number].add_compiled_info(output_file)
+		# Write Terminator to File
+		output_file.write(bytes((0xE5,))) # Terminator

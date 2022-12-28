2b3e57d19770cd2a4c2942c18d245ed55ddcc013
Ada <49806198+NewWorldVulture@users.noreply.github.com>
2022-12-28 05:46:02 -0800

    Updated retry handler

--- bin_loader/src/main.rs
+++ bin_loader/src/main.rs
@@ -1,4 +1,5 @@
-// Created by Ada (NewWorldVulture)
+// Created by NewWorldVulture (Ada MacLurg)
+
 
 extern crate hidapi;
 use std::io;
@@ -17,20 +18,34 @@ fn main() -> io::Result<()> {
     f.read_to_end(&mut buffer)?;
 
     let api = hidapi::HidApi::new().unwrap();
-    // Select GENOVATION Keypad by VID, PID
+    // Select GENOVATION Keypad by VendorID, ProductID
+    // If I ever need to program a different product
+    // ...we need to change the values here
     let (vid, pid) = (0x1125, 0x1807);
     let device = api.open(vid, pid).unwrap();
 
     let mut completed: bool = true;
     for byte in &buffer {
-        // First byte is SET_REPORT ID. We only ever use 0x0
+        // 0u8 -> SET_REPORT ID. We only ever use 0x0
+        // 1u8 -> Marks data as config data
+        // *byte -> Byte of Data
         let sm_buff = [0u8, 1u8, *byte, 0u8, 0u8, 0u8];
-        let result = device.send_feature_report(&sm_buff);
 
-        match result {
-            Ok(_) => { },
-            Err(e) => { eprintln!("{:?}", e);
-                        completed = false; },
+        // Try to send this byte up to 5 times
+        let mut tries: u8 = 0;
+        let mut byte_sent: bool = false;
+        while !byte_sent && tries < 5 {
+            let result = device.send_feature_report(&sm_buff);
+
+            match result {
+                Ok(_)  => { byte_sent = true;
+                            break; },
+                Err(e) => { eprintln!("{:?}", e); },
+            }
+            tries += 1;
+            if tries == 5 {
+                completed = false;
+            }
         }
     }
 
